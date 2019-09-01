782fbb686529d75885e377a1c0565ae17392f5fc
Stefan oude Lohuis <SJ.oudeLohuis@student.han.nl>
2019-09-01 02:19:21 +0200

    Fix

--- Program.cs
+++ Program.cs
@@ -9,17 +9,13 @@ namespace Commandline_Game
     {
         public static void Main(string[] args)
         {
-            // Startup startup = new Startup();
-            // startup.doFakeStartup();
+            Startup startup = new Startup();
+            startup.doFakeStartup();
 
-            // StartMenu startMenu = new StartMenu();
-            // startMenu.showMenuOptions();
-            // //All takeover() methods will pretty much take over everything that happends in the application.
-            // startMenu.takeOver();
-
-            GameMenu gameMenu = new GameMenu();
-            gameMenu.showMenuOptions();
-            gameMenu.takeOver();
+            StartMenu startMenu = new StartMenu();
+            startMenu.showMenuOptions();
+            //All takeover() methods will pretty much take over everything that happends in the application.
+            startMenu.takeOver();
         }
     }
 }
